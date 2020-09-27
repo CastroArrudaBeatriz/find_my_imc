7abfecc97e20fe14d0b9b4cda2dfd7608f3e3a9b
B. Arruda de Castro <b.arruda.de.castro@avanade.com>
2020-09-27 03:20:55 -0300

    colocando mascar e trocar icone

--- app/src/main/java/com/example/find_my_imc/MainActivity.kt
+++ app/src/main/java/com/example/find_my_imc/MainActivity.kt
@@ -5,7 +5,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import java.text.DecimalFormat
 import kotlin.math.pow
 
 
@@ -14,13 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        height_field.addTextChangedListener(MaskEditUtil.mask(height_field, MaskEditUtil.FORMAT_DECIMAL_HEIGHT));
+        weight_field.addTextChangedListener(MaskEditUtil.mask(weight_field, MaskEditUtil.FORMAT_DECIMAL_WEIGHT));
+
         calculate_button.setOnClickListener {
             var imc: Double
             var height = if (height_field.text.toString().isNullOrEmpty()) 0.0 else height_field.text.toString().toDouble()
             var weight = if (weight_field.text.toString().isNullOrEmpty()) 0.0 else weight_field.text.toString().toDouble()
             imc = weight.div(height.pow(2))
-            imc_display.setText(getString(R.string.imc_display_text) +"   "+imc)
-
+            imc_display.setText(getString(R.string.imc_display_text) +"   "+ String.format("%.1f", imc))
         }
 
         clear_button.setOnClickListener {
