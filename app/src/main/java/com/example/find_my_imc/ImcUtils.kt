8f9e25a1ac5f5933a1f7bcd57323ea4fe5720825
B. Arruda de Castro <b.arruda.de.castro@avanade.com>
2020-09-27 18:13:13 -0300

    isolando operações utils

---
+++ app/src/main/java/com/example/find_my_imc/ImcUtils.kt
@@ -0,0 +1,106 @@
+package com.example.find_my_imc
+
+import android.content.Context
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import kotlin.math.pow
+
+object ImcUtil {
+
+    /**
+     * Método para calculor de imc
+     *
+     * @param height_field
+     * @param weight_field
+     * @return imc
+     */
+    fun calculateImc(height_field: EditText , weight_field: EditText): Double{
+        var imc: Double
+        var height = if (height_field.text.toString().isNullOrEmpty()) 0.0 else height_field.text.toString().toDouble()
+        var weight = if (weight_field.text.toString().isNullOrEmpty()) 0.0 else weight_field.text.toString().toDouble()
+        imc = weight.div(height.pow(2))
+        return imc
+    }
+
+    /**
+     * Método para escolher a faixa de imc
+     *
+     * @param slimness_display
+     * @param  normal_display
+     * @param overweight_display
+     * @param obesity_display
+     * @param gravity_obesity_display
+     * @return void
+     * */
+     fun chooseImcStep(slimness_display: Button,
+                              normal_display: Button,
+                              overweight_display: Button,
+                              obesity_display: Button,
+                              gravity_obesity_display: Button,
+                              applicationContext: Context,
+                              imc: Double){
+
+        clearButtonsColor(slimness_display,
+            normal_display,
+            overweight_display,
+            obesity_display,
+            gravity_obesity_display,
+            applicationContext)
+
+        when {
+            imc < 18.5 -> {
+                changeButtonColor(slimness_display, false, applicationContext)
+            }
+            imc in 18.5..24.9 -> {
+                changeButtonColor(normal_display, false, applicationContext)
+            }
+            imc in 25.0..29.9 -> {
+                changeButtonColor(overweight_display, false, applicationContext)
+            }
+            imc in 30.0..39.9 -> {
+                changeButtonColor(obesity_display, false, applicationContext)
+            }
+            imc > 40.0 -> {
+                changeButtonColor(gravity_obesity_display, false, applicationContext)
+            }
+        }
+    }
+
+    /**
+     * Método para resetar a cor default dos botões de niveis do imc
+     *
+     * @param slimness_display
+     * @param  normal_display
+     * @param overweight_display
+     * @param obesity_display
+     * @param gravity_obesity_display
+     * @return void
+     * */
+     fun clearButtonsColor(slimness_display: Button,
+                                  normal_display: Button,
+                                  overweight_display: Button,
+                                  obesity_display: Button,
+                                  gravity_obesity_display: Button,
+                                  applicationContext: Context){
+
+        changeButtonColor(slimness_display, true, applicationContext)
+        changeButtonColor(normal_display, true, applicationContext)
+        changeButtonColor(overweight_display, true, applicationContext)
+        changeButtonColor(obesity_display, true, applicationContext)
+        changeButtonColor(gravity_obesity_display, true, applicationContext)
+     }
+
+    /**
+     * Método para modificar a cor do botão para em destaque ou não destaque
+     *
+     * @param button
+     * @param isClear
+     * @param applicationContext
+     * @return void
+     * **/
+    fun changeButtonColor(button: Button, isClear: Boolean, applicationContext: Context){
+        if(isClear) button.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorDisableButton))
+        else button.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.colorAccent))
+    }
+}
--- app/src/main/java/com/example/find_my_imc/MainActivity.kt
+++ app/src/main/java/com/example/find_my_imc/MainActivity.kt
@@ -19,59 +19,32 @@ class MainActivity : AppCompatActivity() {
         weight_field.addTextChangedListener(MaskEditUtil.mask(weight_field, MaskEditUtil.FORMAT_DECIMAL_WEIGHT));
 
         calculate_button.setOnClickListener {
-            var imc = calculateImc()
+
+            var imc = ImcUtil.calculateImc(height_field,weight_field)
             imc_display.text = getString(R.string.imc_display_text) +"   "+ String.format("%.1f", imc)
-            chooseImcStep(imc)
+
+            ImcUtil.chooseImcStep(slimness_display,
+                normal_display,
+                overweight_display,
+                obesity_display,
+                gravity_obesity_display,
+                applicationContext,
+                imc)
         }
 
         clear_button.setOnClickListener {
+
             height_field.text.clear()
             weight_field.text.clear()
             imc_display.text = getString(R.string.imc_display_text)
-            clearButtonsColor()
+
+            ImcUtil.clearButtonsColor(slimness_display,
+                normal_display,
+                overweight_display,
+                obesity_display,
+                gravity_obesity_display,
+                applicationContext)
         }
-    }
-
-    private fun calculateImc(): Double{
-        var imc: Double
-        var height = if (height_field.text.toString().isNullOrEmpty()) 0.0 else height_field.text.toString().toDouble()
-        var weight = if (weight_field.text.toString().isNullOrEmpty()) 0.0 else weight_field.text.toString().toDouble()
-        imc = weight.div(height.pow(2))
-        return imc
-    }
-
-    private fun chooseImcStep(imc: Double){
-        clearButtonsColor()
-        when {
-            imc < 18.5 -> {
-                changeButtonColor(slimness_display, false)
-            }
-            imc in 18.5..24.9 -> {
-                changeButtonColor(normal_display, false)
-            }
-            imc in 25.0..29.9 -> {
-                changeButtonColor(overweight_display, false)
-            }
-            imc in 30.0..39.9 -> {
-                changeButtonColor(obesity_display, false)
-            }
-            imc > 40.0 -> {
-                changeButtonColor(gravity_obesity_display, false)
-            }
-        }
-    }
-
-    private fun clearButtonsColor(){
-        changeButtonColor(slimness_display, true)
-        changeButtonColor(normal_display, true)
-        changeButtonColor(overweight_display, true)
-        changeButtonColor(obesity_display, true)
-        changeButtonColor(gravity_obesity_display, true)
-    }
-
-    private fun changeButtonColor(button: Button, isClear: Boolean){
-        if(isClear) button.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorDisableButton))
-        else button.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.colorAccent))
     }
 
 }
